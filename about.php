631acac6952705f4945406d0c9769a0b4cc7aacb
kinggiddy1 <kinggiddy@gmail.com>
2025-12-18 22:09:17 +0200

    aded favicon

--- about.php
+++ about.php
@@ -11,8 +11,8 @@ require_once('perlConfig.php');
   <meta name="description" content="">
   <meta name="keywords" content="">
 
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/img/logo/logo.jpg" rel="icon">
+  <link href="assets/img/logo/logo.jpg" rel="apple-touch-icon">
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
